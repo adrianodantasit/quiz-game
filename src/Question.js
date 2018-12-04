9fc6f934f26b953b9f24fd5c45bb1d7d12f01eaf
Adriano <adrianodantasit@gmail.com>
2018-12-04 08:04:54 -0200

    Added storing correct and incorrect scores in the state and their handlers with the UI

--- src/Question.js
+++ src/Question.js
@@ -1,28 +1,57 @@
 import React from "react";
 
-const Question = ({ questions, current, handleClick }) => {
-  return (
-    <div className="card" key={current}>
-      <div className="card__question">
-        <p> {questions[current].question}</p>
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shuffleAnswer = array => {
+    // Using the Fisher–Yates shuffle Algorithm
+    let temporary, randomIndex;
+
+    for (let currentIndex = array.length; currentIndex > 0; ) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporary = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporary;
+    }
+
+    return array;
+  };
+
+  render() {
+    const { questions, current, handleAnswer } = this.props;
+
+    let arrayAnswers = [
+      questions[current].correct_answer,
+      ...questions[current].incorrect_answers //Spread Operator
+    ];
+
+    arrayAnswers = this.shuffleAnswer(arrayAnswers);
+
+    return (
+      <div className="card" key={current}>
+        <div className="card__question">
+          <p> {questions[current].question}</p>
+        </div>
+        <div className="card__answers">
+          {arrayAnswers.map((answer, i) => {
+            return (
+              <p
+                key={answer}
+                className={`card__answers--${i}`}
+                onClick={e => handleAnswer(current, answer)}
+              >
+                {i}) {answer}
+              </p>
+            );
+          })}
+        </div>
       </div>
-      <div className="card__answers">
-        <p className="card__answers--1">
-          a) {questions[current].correct_answer}
-        </p>
-        <p className="card__answers--2">
-          b) {questions[current].incorrect_answers[0]}
-        </p>
-        <p className="card__answers--3">
-          c) {questions[current].incorrect_answers[1]}
-        </p>
-        <p className="card__answers--4">
-          d) {questions[current].incorrect_answers[2]}
-        </p>
-      </div>
-      <button onClick={() => handleClick(current)}>Next</button>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Question;
--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import "../src/sass/main.scss";
 import Question from "./Question";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       questions: [],
-      current: 0
+      current: 0,
+      correct: 0,
+      incorrect: 0
     };
   }
 
@@ -29,7 +31,14 @@ class App extends React.Component {
       );
   };
 
-  handleClick = current => {
+  handleAnswer = (current, answer) => {
+    if (this.state.questions[current].correct_answer === answer) {
+      this.setState({ correct: this.state.correct + 1 });
+    } else if (
+      this.state.questions[current].incorrect_answers.includes(answer)
+    ) {
+      this.setState({ incorrect: this.state.incorrect + 1 });
+    }
     this.setState({ current: current + 1 });
   };
 
@@ -37,13 +46,13 @@ class App extends React.Component {
     const { questions, current } = this.state;
 
     if (questions.length > 0 && current < 10) {
-      console.log(questions, current);
+      console.log(this.state);
       return (
         <div className="interface">
           <Question
             questions={questions}
             current={current}
-            handleClick={this.handleClick}
+            handleAnswer={this.handleAnswer}
           />
         </div>
       );

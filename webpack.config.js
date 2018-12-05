b7c0b66ccc4f010363ff4137dd5617cf72183249
Adriano <adrianodantasit@gmail.com>
2018-12-05 11:18:14 -0200

    Updating Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/app.js"],
+  entry: ["@babel/polyfill", "./src/js/Containers/App.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/bundle.js"
